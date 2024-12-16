f9c54fc49dc0cf4896ad3ff3b00e0dbb71826810
Haya Elnaggar <haya.ahmed05@eng-st.cu.edu.eg>
2024-12-17 00:15:48 +0200

    DisplayRandomCommands
    implemented class Display Random Commands
    (to be tested)
    (revise Friend declaration in output class)

--- Action.cpp
+++ Action.cpp
@@ -1,5 +1,6 @@
 #include "Action.h"
-
+# pragma once
+# include <string>
 Action::Action(ApplicationManager *pApp) : pManager(pApp)
 {
 	// The constructor initializes the ApplicationManager pointer data member
