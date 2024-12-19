5cfb468e97f4da65465f059a423eb85b5a22de90
waleyeldeen <waleyeldeenahmed@gmail.com>
2024-12-20 01:12:13 +0200

    Class definition

--- AddWorkshopAction.h
+++ AddWorkshopAction.h
@@ -1,7 +1,25 @@
 #pragma once
 #include "Action.h"
+#include "Input.h"
+#include "Output.h"
+#include "Workshop.h"
+
 class AddWorkshopAction :
     public Action
 {
+private:
+    // Always add action parameters as private data members
+
+    // [Action Parameters]
+    CellPosition workshopPos; // The position of the water pit
+
+    // Note: These parameters should be read in ReadActionParameters()
+public:
+    AddWorkshopAction(ApplicationManager* pApp);
+
+    virtual void ReadActionParameters();
+
+    virtual void Execute();
+
 };
 
