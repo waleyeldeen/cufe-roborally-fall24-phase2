b6226fdb9370d259c619dd6e730992d19122bc1c
waleyeldeen <waleyeldeenahmed@gmail.com>
2024-12-20 01:12:39 +0200

    Implement functions

--- AddWorkshopAction.cpp
+++ AddWorkshopAction.cpp
@@ -1,1 +1,36 @@
 #include "AddWorkshopAction.h"
+
+AddWorkshopAction::AddWorkshopAction(ApplicationManager* pApp) : Action(pApp) {}
+
+
+void AddWorkshopAction::ReadActionParameters() {
+	// Get a Pointer to the Input / Output Interfaces
+	Grid* pGrid = pManager->GetGrid();
+	Output* pOut = pGrid->GetOutput();
+	Input* pIn = pGrid->GetInput();
+
+	// Read the waterPitPos parameter
+	pOut->PrintMessage("New water pit: Click on Target Cell");
+	workshopPos = pIn->GetCellClicked();
+}
+
+
+void AddWorkshopAction::Execute() {
+	ReadActionParameters();
+
+	Workshop* pWorkshop = new Workshop(workshopPos);
+
+	Grid* pGrid = pManager->GetGrid(); // We get a pointer to the Grid from the ApplicationManager
+	Output* pOut = pGrid->GetOutput();
+
+	bool added = pGrid->AddObjectToCell(pWorkshop);
+
+	// if the GameObject cannot be added
+	if (!added)
+	{
+		// Print an appropriate message
+		pGrid->PrintErrorMessage("Error: Cell already has an object ! Click to continue ...");
+	}
+
+	pOut->PrintMessage("Added Workshop at (" + to_string(workshopPos.VCell()) + ", " + to_string(workshopPos.HCell()) + ")");
+}
