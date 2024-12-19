678fb27a4f94bcc9be050d0edf264cc965caf72d
waleyeldeen <waleyeldeenahmed@gmail.com>
2024-12-19 21:19:37 +0200

    Implement ReadActionParameters()

--- AddWaterPitAction.cpp
+++ AddWaterPitAction.cpp
@@ -1,1 +1,23 @@
 #include "AddWaterPitAction.h"
+
+AddWaterPitAction:: AddWaterPitAction(ApplicationManager* pApp) : Action() {}
+
+
+void AddWaterPitAction::ReadActionParameters() {
+	// Get a Pointer to the Input / Output Interfaces
+	Grid* pGrid = pManager->GetGrid();
+	Output* pOut = pGrid->GetOutput();
+	Input* pIn = pGrid->GetInput();
+
+	// Read the waterPitPos parameter
+	pOut->PrintMessage("New water pit: Click on Target Cell");
+	waterPitPos = pIn->GetCellClicked();
+}
+
+
+void AddWaterPitAction::Execute() {
+    ReadActionParameters();
+    //GameObject* pWaterPit = new GameObject(); // Replace with actual WaterPit class
+    //pManager->GetGrid()->AddObjectToCell(pWaterPit, position);
+    //pManager->GetOutput()->PrintMessage("Added Water Pit at (" + std::to_string(position.vCell) + ", " + std::to_string(position.hCell) + ")");
+}
