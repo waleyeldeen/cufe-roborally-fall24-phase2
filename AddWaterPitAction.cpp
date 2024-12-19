e3d7d28153b659780a4f3745c12817cdf9ee0d06
waleyeldeen <waleyeldeenahmed@gmail.com>
2024-12-19 22:36:22 +0200

    Implement execute

--- AddWaterPitAction.cpp
+++ AddWaterPitAction.cpp
@@ -1,6 +1,6 @@
 #include "AddWaterPitAction.h"
 
-AddWaterPitAction:: AddWaterPitAction(ApplicationManager* pApp) : Action() {}
+AddWaterPitAction:: AddWaterPitAction(ApplicationManager* pApp) : Action(pApp) {}
 
 
 void AddWaterPitAction::ReadActionParameters() {
@@ -17,7 +17,21 @@ void AddWaterPitAction::ReadActionParameters() {
 
 void AddWaterPitAction::Execute() {
     ReadActionParameters();
-    //GameObject* pWaterPit = new GameObject(); // Replace with actual WaterPit class
+
+    WaterPit* pWaterPit = new WaterPit(waterPitPos);
+
+	Grid* pGrid = pManager->GetGrid(); // We get a pointer to the Grid from the ApplicationManager
+
+	bool added = pGrid->AddObjectToCell(pWaterPit);
+
+	// if the GameObject cannot be added
+	if (!added)
+	{
+		// Print an appropriate message
+		pGrid->PrintErrorMessage("Error: Cell already has an object ! Click to continue ...");
+	}
+	// Here, the belt is created and added to the GameObject of its Cell, so we finished executing the AddBeltAction
+
     //pManager->GetGrid()->AddObjectToCell(pWaterPit, position);
     //pManager->GetOutput()->PrintMessage("Added Water Pit at (" + std::to_string(position.vCell) + ", " + std::to_string(position.hCell) + ")");
 }
