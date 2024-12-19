624aed824a6a9ba44c1bc8c8aee910f79bbb3658
waleyeldeen <waleyeldeenahmed@gmail.com>
2024-12-19 19:28:34 +0200

    Add files for AddWaterPitAction

---
+++ AddWaterPitAction.cpp
@@ -0,0 +1,1 @@
+#include "AddWaterPitAction.h"
---
+++ AddWaterPitAction.h
@@ -0,0 +1,11 @@
+#pragma once
+#include "Action.h"
+#include "Input.h"
+#include "Output.h"
+#include "Belt.h"
+
+class AddWaterPitAction :
+    public Action
+{
+};
+
