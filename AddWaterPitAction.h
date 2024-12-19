aa344ea1c29ee6e47eaf6e28477a1c27e8332290
waleyeldeen <waleyeldeenahmed@gmail.com>
2024-12-19 21:17:46 +0200

    Add data members and function prototypes

--- AddWaterPitAction.h
+++ AddWaterPitAction.h
@@ -4,8 +4,18 @@
 #include "Output.h"
 #include "Belt.h"
 
-class AddWaterPitAction :
-    public Action
-{
-};
+class AddWaterPitAction : public Action {
+private:
+    // Always add action parameters as private data members
 
+    // [Action Parameters]
+    CellPosition waterPitPos; // The position of the water pit
+
+    // Note: These parameters should be read in ReadActionParameters()
+public:
+    AddWaterPitAction(ApplicationManager* pApp);
+
+    virtual void ReadActionParameters();
+
+    virtual void Execute();
+};
