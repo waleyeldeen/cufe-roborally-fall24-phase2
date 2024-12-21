f90f613725929138a683b5eec102b43db7baf834
Haya Elnaggar <haya.ahmed05@eng-st.cu.edu.eg>
2024-12-21 23:12:18 +0200

    changed implementation without get action

--- ShootingPhaseAction.h
+++ ShootingPhaseAction.h
@@ -5,9 +5,10 @@
 class ShootingPhaseAction
 {
 	Grid* pGrid;
-	bool IsFacingOtherPlayer(const CellPosition& shooterPos, Direction shooterDir, const CellPosition& targetPos) const;
+	
 public:
 	ShootingPhaseAction(Grid* grid);
 	void Execute();
+	~ShootingPhaseAction();
 };
 
