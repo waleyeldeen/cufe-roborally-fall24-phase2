1e8b6faedd2b42c28d06f4a177ed3302022625a4
waleyeldeen <waleyeldeenahmed@gmail.com>
2024-12-22 01:42:29 +0200

    formatting

--- AddRotatingGearAction.cpp
+++ AddRotatingGearAction.cpp
@@ -7,10 +7,7 @@ AddRotatingGearAction::AddRotatingGearAction(ApplicationManager * pApp):Action(p
 
 void AddRotatingGearAction::ReadActionParameters()
 {
-	///TODO: Implement this function as mentioned in the guideline steps (numbered below) below
-
-
-	// == Here are some guideline steps (numbered below) to implement this function ==
+	///[+]TODO: Implement this function as mentioned in the guideline steps (numbered below) below
 
 	// 1- Get a Pointer to the Input / Output Interfaces
 	Grid* pGrid = pManager->GetGrid();
@@ -34,18 +31,18 @@ void AddRotatingGearAction::ReadActionParameters()
 
 void AddRotatingGearAction::Execute()
 {
-	// The first line of any Action Execution is to read its parameter first 
-	// and hence initializes its data members
-	ReadActionParameters();
 	///[+]TODO: Implement this function as mentioned in the guideline steps (numbered below) below
-	// == Here are some guideline steps (numbered below) to implement this function ==
+	ReadActionParameters();
 
 	// 1-Create a rotating gear object
 	RotatingGear* pGear = new RotatingGear(gearPos, clockwise);
+
 	// 2-get a pointer to the Grid from the ApplicationManager
 	Grid* pGrid = pManager->GetGrid();
+
 	// 3-Add the rotating object to the GameObject of its Cell:
 	bool added = pGrid->AddObjectToCell(pGear);
+
 	// 4-Check if the rotating gear was added and print an errror message if flag couldn't be added
 	if (!added)
 	{
