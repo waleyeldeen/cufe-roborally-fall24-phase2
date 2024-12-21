59f294d97e4c7a043644191657c2f453a2d01971
Haya Elnaggar <haya.ahmed05@eng-st.cu.edu.eg>
2024-12-21 23:37:30 +0200

    included enum for shootingPhase
    to be implemented

--- DEFS.h
+++ DEFS.h
@@ -23,7 +23,7 @@ enum ActionType // The actions supported (add more as needed)
 	ADD_WATER_PIT,
 	ADD_ROTATING_GEAR,
 	ADD_WORKSHOP,
-
+	SHOOTING_PHASE,
 
 	///TODO: Add more action types of Design Mode
 
