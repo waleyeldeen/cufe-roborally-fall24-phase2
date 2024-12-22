b538a5b205e9e64a405a047f167dbef9b43c17a8
MennaMohamed315 <mennamohmedaly211@gmail.com>
2024-12-22 21:59:59 +0200

    added save function

--- WaterPit.h
+++ WaterPit.h
@@ -7,7 +7,7 @@ public:
 	virtual void Draw(Output* pOut) const; // Draws a water pit in the cell
 
 	virtual void Apply(Grid* pGrid, Player* pPlayer); // Applys the effect of the waterPit by killing the robot instantly and ending the game
-	void Save(GType type) const override;
+	void Save(ofstream &outfile,GType type) const ;
 
 	virtual ~WaterPit();
 };
