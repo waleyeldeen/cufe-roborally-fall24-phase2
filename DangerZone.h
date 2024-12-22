b8382022e982fc3e3c3b2f5b10e60c73ed1418f2
MennaMohamed315 <mennamohmedaly211@gmail.com>
2024-12-22 21:57:48 +0200

    added save function

--- DangerZone.h
+++ DangerZone.h
@@ -8,7 +8,7 @@ public:
 	virtual void Draw(Output* pOut) const; // Draws a danger zone in the cell
 
 	virtual void Apply(Grid* pGrid, Player* pPlayer); // Applys the effect of the danger zone by reducing the robot's health by 1
-	void Save(GType type) const override;
+	void Save(ofstream &outfile,GType type) const ;
 
 
 	virtual ~DangerZone();
