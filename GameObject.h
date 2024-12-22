68389db784d306cb2d403cd02db6c52fb0c073b3
MennaMohamed315 <mennamohmedaly211@gmail.com>
2024-12-23 00:47:09 +0200

    Update GameObject.h
    added load function and gameobject types

--- GameObject.h
+++ GameObject.h
@@ -1,7 +1,17 @@
 #pragma once
 
 #include "Grid.h"
+enum GType 
+{
+	FLAG,
+	WATER_PIT,
+	DANGER_ZONE,
+	BELT,
+	WORKSHOP,
+	ANTENNA,
+	ROTATING_GEAR
 
+};
 
 // Base Class for All Game Objects ( Belts, danger zones, .. )
 class GameObject
@@ -35,6 +45,7 @@ public:
 	//virtual void Save(ofstream &OutFile) = 0;	// Saves the GameObject parameters to the file
 	//virtual void Load(ifstream &Infile) = 0;	// Loads and Reads the GameObject parameters from the file
 
+	virtual void  Load(ifstream& infile, GType type);
 	virtual ~GameObject(); // Virtual destructor
 };
 
