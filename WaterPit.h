49cb8f249d0dd2690ab7115a40a85a4953381695
MennaMohamed315 <mennamohmedaly211@gmail.com>
2024-12-23 01:26:09 +0200

    added load function

--- WaterPit.h
+++ WaterPit.h
@@ -7,7 +7,7 @@ public:
 	virtual void Draw(Output* pOut) const; // Draws a water pit in the cell
 
 	virtual void Apply(Grid* pGrid, Player* pPlayer); // Applys the effect of the waterPit by killing the robot instantly and ending the game
-
+	virtual void  load(ifstream& infile, GType type) const;
 
 	virtual ~WaterPit();
 };
