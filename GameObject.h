adf12c4b4b46ffe48be4ede9e1d93c5aa479367d
MennaMohamed315 <mennamohmedaly211@gmail.com>
2024-12-22 21:58:30 +0200

    added save function

--- GameObject.h
+++ GameObject.h
@@ -44,7 +44,7 @@ public:
 	//Uncomment those functions and implement them in all the derived classes 
 	//virtual void Save(ofstream &OutFile) = 0;	// Saves the GameObject parameters to the file
 	//virtual void Load(ifstream &Infile) = 0;	// Loads and Reads the GameObject parameters from the file
-	virtual void  Save(std::ofstream file,GType type) const=0;
+	virtual void  Save(ofstream &outfile,GType type) const;
 	virtual ~GameObject(); // Virtual destructor
 };
 
