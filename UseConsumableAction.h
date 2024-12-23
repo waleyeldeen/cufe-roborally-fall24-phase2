963201fc9a8f39d345c9def9bd371a287b0cc9d0
waleyeldeen <waleyeldeenahmed@gmail.com>
2024-12-23 15:39:49 +0200

    define header

--- UseConsumableAction.h
+++ UseConsumableAction.h
@@ -1,7 +1,22 @@
 #pragma once
 #include "Action.h"
-class UseConsumableAction :
-    public Action
+#include "Grid.h"
+#include "Input.h"
+#include "Output.h"
+
+class UseConsumableAction : public Action
 {
+private:
+	Consumable consumable;
+    Consumable list[CONSUMABLE_COUNT];
+    bool validParam;
+public:
+    UseConsumableAction(ApplicationManager* pApp);
+
+    virtual void ReadActionParameters();
+
+    virtual void Execute();
+
+    virtual ~UseConsumableAction();
 };
 
