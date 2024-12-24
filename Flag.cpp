c535f063be0e708b79c7da9341a7fd78bcd20746
MennaMohamed315 <mennamohmedaly211@gmail.com>
2024-12-24 20:41:22 +0200

    edited flag function

--- Flag.cpp
+++ Flag.cpp
@@ -12,16 +12,17 @@ void Flag::Draw(Output* pOut) const
 void Flag::Apply(Grid* pGrid, Player* pPlayer)
 {
 	Output* pOut;
+	Input* pIn;
 	pOut->PrintMessage("You have reached a flag .click to continue");
+	pIn->GetCellClicked();
 	
-	pGrid->GetInput()->GetCellClicked();
 	
 	///TODO: Implement this function as mentioned in the guideline steps (numbered below) below
 	// == Here are some guideline steps (numbered below) to implement this function ==
-	pOut->PrintMessage("End");
+	pOut->PrintMessage("Endgame");
 	pGrid->SetEndGame(true);
 	// 1- Print a message "You have reached a flag. Click to continue ..." and wait mouse click
-
+	
 	// 2- Apply the flag's effect by ending the game
 	//    Review the "pGrid" functions and decide which function can be used for that
 }
