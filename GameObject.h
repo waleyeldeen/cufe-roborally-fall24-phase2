cedd27ef03deb6019365b5d460d1730f3e9a6e3c
MennaMohamed315 <mennamohmedaly211@gmail.com>
2024-12-24 21:56:50 +0200

    nothing changed

--- GameObject.h
+++ GameObject.h
@@ -24,7 +24,7 @@ public:
 	virtual void Draw(Output* pOut) const = 0;	 // Draws the game object in the window in his position cell 
 											     // (drawing depends on GameObject Type, so virtual)
 
-	virtual void Apply(Grid* pGrid, Player* pPlayer) = 0;  // Applys the effect of the GameObject on the passed Player
+	virtual void Apply(Grid * pGrid, Player * pPlayer) = 0;  // Applys the effect of the GameObject on the passed Player
 	                                                       // (The effect depends on the GameObject type, so virtual)
 	                                                       // For example, applying a belt is by moving player to the 
 														   // end of the belt, and so on
